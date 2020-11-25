622b2ddeaf81642c4b8217003a09ded8f5df4f0c
Jörg Thalheim <joerg@thalheim.io>
2020-11-25 20:14:46 +0000

    update poetry2nix git lock

--- pkgs/poetry-git-overlay.nix
+++ pkgs/poetry-git-overlay.nix
@@ -1,20 +1,11 @@
 { pkgs }:
 self: super: {
 
-  intervaltree = super.intervaltree.overridePythonAttrs (
-    _: {
-      src = pkgs.fetchzip {
-        url = "https://github.com/chaimleib/intervaltree/archive/6a731f2ce9f658cd6b8da2f7e23e96642643be54.zip";
-        sha256 = "1rn5w2j9ap5jh7lx8cqr23ssx7pcmp8f2y80r2h1di031wy1lf71";
-      };
-    }
-  );
-
   pwntools = super.pwntools.overridePythonAttrs (
     _: {
       src = pkgs.fetchzip {
-        url = "https://github.com/hase-project/pwntools/archive/51645530f281930c03936a3a1cd886e0ed481bc3.zip";
-        sha256 = "1fimgx46h468877cfq2qfnd795sllzfpiar71jh368sfxb87xr39";
+        url = "https://github.com/hase-project/pwntools/archive/59fbeb767ede61c8a54292daad3fa07cb996b805.tar.gz";
+        sha256 = "1mcz0sifici186w67kbrks0qm2y3w35c1z40xx85g51gn88i5z28";
       };
     }
   );
